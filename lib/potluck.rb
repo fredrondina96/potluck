a4ac92894af5bb69027462303d246a4031abc643
Fred Rondina <fredrondina96@gmail.com>
2019-12-02 11:17:49 -0700

    Added tests for potluck dish storing

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -2,10 +2,14 @@ require 'pry'
 
 class Potluck
 
-attr_reader :date, :category
+attr_reader :date, :dishes
 
   def initialize(date, dishes = [])
     @date = date
     @dishes = dishes
+  end
+
+  def add_dish(new_dish)
+    @dishes << new_dish
   end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -8,6 +8,8 @@ require 'pry'
 class PotluckTest < Minitest::Test
 
   def setup
+    @couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
     @potluck = Potluck.new("7-13-18")
   end
 
@@ -16,8 +18,13 @@ class PotluckTest < Minitest::Test
   end
 
   def test_potluck_stores_date
-    binding.pry
     assert_equal "7-13-18", @potluck.date
   end
 
+  def test_potluck_stores_dishes
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@cocktail_meatballs)
+    binding.pry
+    assert_equal 2, @potluck.dishes.length
+  end
 end
